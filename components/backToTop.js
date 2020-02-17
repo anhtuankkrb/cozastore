69beedb06bfa78818196a4fcbb74f59736dc8866
anhtuankkrb <anhtuankkrb@gmail.com>
2020-02-17 19:05:42 +0700

    delete

--- components/backToTop.js
+++
@@ -1,9 +0,0 @@
-export default function BackToTop() {
-  return (
-    <div className="btn-back-to-top" id="myBtn">
-      <span className="symbol-btn-back-to-top">
-        <i className="zmdi zmdi-chevron-up" />
-      </span>
-    </div>
-  );
-}
